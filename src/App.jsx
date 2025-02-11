a25d19540ae3c8a07ec05b90d1e94a833f716ad0
moldenburger <maritoldenburger@hotmail.com>
2025-02-11 15:18:48 +0100

    Finished buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
-import './App.css'
+import './App.css';
+import Button from "./components/Button.jsx";
+import bag1 from "./assets/bag_1.png"
+import bag2 from "./assets/bag_2.png"
+import bag3 from "./assets/bag_3.png"
+import bag4 from "./assets/bag_4.png"
+
 
 function App() {
-  return (
-      <h1>Handbags & Purses</h1>
-  )
+    return (
+        <>
+            <h1>Handbags & Purses</h1>
+            <nav>
+                <Button
+                    buttonText="to the collection"
+                    disabledButton=""
+                />
+                <Button
+                    buttonText="shop all bags"
+                />
+                <Button
+                    buttonText="pre-orders"
+                    disabledButton={true}
+                />
+            </nav>
+
+            <main>
+                <article>
+                    <span>Best seller</span>
+                    <img src={bag1} alt="The handy bag"/>
+                    <p>The handy bag</p>
+                </article>
+            </main>
+        </>
+    )
 }
 
 export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+function Button({buttonText, disabledButton}) {
+    return (
+        <button onClick={() => {
+            console.log(buttonText);
+        }} disabled={disabledButton}>{buttonText}</button>
+    )
+}
+
+export default Button
